688e8b69705f7d432798f72c3634b3fd519c163f
marineorganism <marineorganism@gmail.com>
2015-12-17 20:16:46 +1100

    filled in the html on the message page

---
+++ bad.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>Santa Says..</title>
+		<link rel="stylesheet" href="christmasCheer.css">
+	</head>
+	<body>
+		<div>
+			<h1>BAD!</h1>
+			<h2>Satan is not at all impressed, here's what he had to say on the matter</h2>
+			<a>
+				<?php
+					echo "yee";
+				?>
+			<a/>
+		</div>
+	</body>
+</head>
